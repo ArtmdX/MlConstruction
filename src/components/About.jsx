5a1b9efa0e4824cc24192646899484b1dcc3da3f
Arthur Mauricio <arthur.mdx1234@gmail.com>
2025-03-09 21:15:34 -0300

    Overflow bug solved

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,13 +1,19 @@
 export default function About() {
   return (
-    <section id="about" className="h-auto py-30 flex items-start justify-center bg-gray-100">
+    <section
+      id="about"
+      className="h-auto py-30 flex items-start justify-center bg-gray-100"
+    >
       <div className="max-w-4/5 text-center">
         <h2 className="text-4xl font-bold text-blue-600">About us</h2>
-        <p className="mt-20 p-3 text-center  m-5 font-semibold text-2xl">
-          ML Construction is committed to excellence in every aspect of our business. We uphold a standard of integrity
-          bound by fairness, honesty, and personal responsibility. Our distinction is the quality of service we bring to
-          our customers. Accurate knowledge of our trade combined with ability is what makes us true professionals.
-          Above all, we are watchful of our customers' interests, and make their concerns the basis of our business.
+        <p className="mt-20 p-3 hyphens-auto text-justify m-5 font-semibold text-2xl">
+          MLConstruction is committed to excellence in every aspect of our
+          business. We uphold a standard of integrity bound by fairness,
+          honesty, and personal responsibility. Our distinction is the quality
+          of service we bring to our customers. Accurate knowledge of our trade
+          combined with ability is what makes us true professionals. Above all,
+          we are watchful of our customers' interests, and make their concerthe
+          basis of our business.
         </p>
       </div>
     </section>
--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,12 +6,12 @@ export default function Introduction() {
   return (
     <section
       id="intro"
-      className={`h-screen flex items-center ${
+      className={`h-screen w-full flex items-center ${
         windowWidth < 768 ? "bg-header-mobile" : "bg-header-desktop"
       } bg-cover bg-center px-8`}>
       <div className="text-white md:bg-blue-950 p-5 rounded-2xl">
-        <h1 className="text-5xl font-bold">MLConstruction</h1>
-        <h2 className="text-2xl mt-4">Delivering Excellence in Every Build</h2>
+        <h1 className="text-3xl md:text-5xl font-bold max-w-screen">MLConstruction</h1>
+        <h2 className="text-xl mt-4 max-w-screen">Delivering Excellence in Every Build</h2>
       </div>
     </section>
   );
